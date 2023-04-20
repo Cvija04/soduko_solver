1b298a8c17c6fae813ba5f75ecdd67e17250c192
Cvija04 <32720856+Cvija04@users.noreply.github.com>
2023-04-20 21:29:57 +0200

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from .views import SolveBoardView, GenerateBoardView
 
 urlpatterns = [
     path('solver/', SolveBoardView.as_view(), name='solver'),
-    path('random/', GenerateBoardView.as_view(), name='random')
+    path('generate/', GenerateBoardView.as_view(), name='generate_board')
 ]
